1703e7b41c7ef65d32b1168bfaca30e0558cec66
lvertiz4 <lvertiz@gmail.com>
2023-10-01 18:08:21 -0400

    started with greet hello world function handler

--- src/main.rs
+++ src/main.rs
@@ -1,3 +1,24 @@
-fn main() {
-    println!("Hello, world!");
+use axum::{Router, routing::get, http::Uri, response::IntoResponse};
+
+async fn greet(uri: Uri) -> impl IntoResponse {
+  let mut name = uri.path().to_owned();
+  if name == "/".to_owned() {
+    format!("Hello, World!")
+    } else {
+    let _ = name.remove(0);
+    format!("Hello, {}!", &name)
+    }
+}
+
+#[tokio::main]
+async fn main() {
+    let app = Router::new()
+        .route("/", get(greet))
+        .route("/:uri", get(greet));        
+
+    axum::Server::bind(&"0.0.0.0:3000".parse().expect("Server did not start as expected"))
+        .serve(app.into_make_service())
+        .await
+        .unwrap();
+   
 }
