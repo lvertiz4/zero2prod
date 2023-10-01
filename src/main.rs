f57d418e7e812f5ad5286a1c2e565239952f1b33
lvertiz4 <lvertiz@gmail.com>
2023-10-01 19:01:10 -0400

    added health_check function

--- src/main.rs
+++ src/main.rs
@@ -1,4 +1,4 @@
-use axum::{Router, routing::get, http::Uri, response::IntoResponse};
+use axum::{Router, routing::get, http::{Uri, StatusCode}, response::IntoResponse};
 
 async fn greet(uri: Uri) -> impl IntoResponse {
   let mut name = uri.path().to_owned();
@@ -10,11 +10,16 @@ async fn greet(uri: Uri) -> impl IntoResponse {
     }
 }
 
+async fn health_check() -> StatusCode {
+    StatusCode::OK
+}
+
 #[tokio::main]
 async fn main() {
     let app = Router::new()
         .route("/", get(greet))
-        .route("/:uri", get(greet));        
+        .route("/:uri", get(greet))
+        .route("/health_check", get(health_check));        
 
     axum::Server::bind(&"0.0.0.0:3000".parse().expect("Server did not start as expected"))
         .serve(app.into_make_service())
