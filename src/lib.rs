f705157b24abfd5adf2926212dbb2f4447d7c21f
lvertiz4 <lvertiz@gmail.com>
2023-10-26 00:34:24 -0400

    finished 3.7.3.1; axum defaults to 422 not 400

--- src/lib.rs
+++ src/lib.rs
@@ -1,6 +1,7 @@
 use std::net::TcpListener;
-use axum::{Router, routing::{get, IntoMakeService}, http::{Uri, StatusCode}, response::IntoResponse};
+use axum::{Router, routing::{get, IntoMakeService, post}, http::{Uri, StatusCode}, response::IntoResponse, Form};
 use hyper::{Server, server::conn::AddrIncoming};
+use serde::Deserialize;
 
 async fn greet(uri: Uri) -> impl IntoResponse {
   let mut name = uri.path().to_owned();
@@ -16,11 +17,22 @@ async fn health_check() -> StatusCode {
     StatusCode::OK
 }
 
+#[derive(Deserialize)]
+struct FormData {
+    email: String,
+    name: String
+}
+
+async fn subscribe(_form: Form<FormData>) -> StatusCode {
+    StatusCode::OK
+}
+
 pub fn run(listener: TcpListener) -> hyper::Result<Server<AddrIncoming, IntoMakeService<Router>>> {
     let app = Router::new()
         .route("/", get(greet))
         .route("/:uri", get(greet))
-        .route("/health_check", get(health_check));        
+        .route("/health_check", get(health_check))
+        .route("/subscriptions", post(subscribe));        
 
     let server = axum::Server::from_tcp(listener)
         .expect("Could not instantiate TcpListener")
--- tests/heath_check.rs
+++ tests/heath_check.rs
@@ -4,6 +4,23 @@
 
 use std::net::TcpListener;
 
+//Launch instance of our application in the background
+fn spawn_app() -> String {
+    let listener = TcpListener::bind("127.0.0.1:0")
+    .expect("Could not retrieve TcpListener");
+    // //Retrieve the port number assigned to us by the Operating System
+    let port = listener.local_addr().expect("Could not retrieve local address").port();
+    let server = zero2prod::run(listener)
+    .expect("Failed to bind to address");
+    // Launch the server as a background task
+    // tokio::spawn returns a handle to the spawned future,
+    // but we have no use for it here, hence the non-binding let
+    let _ = tokio::spawn(server);
+    //We return the application address to the caller
+   format!("http://127.0.0.1:{}", port)
+}
+
+
 #[tokio::test]
 async fn health_check_works() {
 //     The test covers the full range of properties we are interested to check:
@@ -27,18 +44,46 @@ async fn health_check_works() {
     assert!(response.status().is_success());
     assert_eq!(Some(0), response.content_length());
 }
-//Launch instance of our application in the background
-fn spawn_app() -> String {
-    let listener = TcpListener::bind("127.0.0.1:0")
-    .expect("Could not retrieve TcpListener");
-    // //Retrieve the port number assigned to us by the Operating System
-    let port = listener.local_addr().expect("Could not retrieve local address").port();
-    let server = zero2prod::run(listener)
-    .expect("Failed to bind to address");
-    // Launch the server as a background task
-    // tokio::spawn returns a handle to the spawned future,
-    // but we have no use for it here, hence the non-binding let
-    let _ = tokio::spawn(server);
-    //We return the application address to the caller
-   format!("http://127.0.0.1:{}", port)
+
+#[tokio::test]
+async fn subscribe_returns_a_200_for_valid_form_data() {
+    //Set up instance of web serve application and testing Client instance
+    let app_address = spawn_app();
+    let client = reqwest::Client::new();
+    //Act
+    let body = "name=luis%20vertiz&email=lvertiz%40gmail.com";
+    let response = client
+        .post(&format!("{}/subscriptions", &app_address))
+        .header("Content-Type", "application/x-www-form-urlencoded")
+        .body(body)
+        .send()
+        .await
+        .expect("Failed to execute subscription POST request");
+    //Testing
+    assert_eq!(200, response.status().as_u16());
+}
+
+#[tokio::test]
+async fn subscribe_returns_a_400_when_data_is_missing() {
+    let app_address = spawn_app();
+    let client = reqwest::Client::new();
+    let test_cases = vec![
+        ("name=luis%20vertiz", "missing the email"),
+        ("email=lvertiz%40gmail.com", "missing the name"),
+        ("", "missing both name and email")
+    ];
+    for (invalid_body, error_message) in test_cases {
+        let response = client
+            .post(&format!("{}/subscriptions", &app_address))
+            .header("Content-Type", "application/x-www-form-urlencoded")
+            .body(invalid_body)
+            .send()
+            .await
+            .expect("Failed to execute POST Request");
+        
+        assert_eq!(422, response.status().as_u16(),// Axum defaults to 422 instead of 400
+            //Additional customized error message on test failue
+            "The API did not fail with 400 Bad Request when payload was {}.",
+            error_message);
+    }
 }
