457a315c82127e338861a81fecf46b8dc15ac92a
lvertiz4 <lvertiz@gmail.com>
2023-11-01 23:22:49 -0400

    gonna try cargo clippy without -D

--- src/lib.rs
+++ src/lib.rs
@@ -7,7 +7,7 @@ pub mod startup;
 
 pub async fn greet(uri: Uri) -> impl IntoResponse {
     let mut name = uri.path().to_owned();
-    if name == "/".to_owned() {
+    if name == "*/".to_owned() {
         format!("Hello, World!")
     } else {
         let _ = name.remove(0);
