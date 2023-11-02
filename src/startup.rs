52d2291d6e5bb5f8cf822cb13160c0b38ec12860
lvertiz4 <lvertiz@gmail.com>
2023-11-01 23:35:40 -0400

    deleted space after subscribe handler

--- src/startup.rs
+++ src/startup.rs
@@ -15,7 +15,7 @@ pub fn run(listener: TcpListener) -> hyper::Result<Server<AddrIncoming, IntoMake
         .route("/", get(greet))
         .route("/:uri", get(greet))
         .route("/health_check", get(health_check))
-        .route("/subscriptions", post(subscribe));       
+        .route("/subscriptions", post(subscribe));      
 
     let server = axum::Server::from_tcp(listener)
         .expect("Could not instantiate TcpListener")
