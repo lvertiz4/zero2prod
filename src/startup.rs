0ecf51f4b8b0bdbaa8e92fb06dea4ff84716cf5c
lvertiz4 <lvertiz@gmail.com>
2023-11-01 23:40:54 -0400

    round3 fixed spacing for startup.rs

--- src/startup.rs
+++ src/startup.rs
@@ -16,7 +16,7 @@ pub fn run(listener: TcpListener) -> hyper::Result<Server<AddrIncoming, IntoMake
         .route("/:uri", get(greet))
         .route("/health_check", get(health_check))
         .route("/subscriptions", post(subscribe));
-    
+
     let server = axum::Server::from_tcp(listener)
         .expect("Could not instantiate TcpListener")
         .serve(app.into_make_service());
