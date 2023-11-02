a555f11903a19e4890a75e458abb6e4a0dcdc08f
lvertiz4 <lvertiz@gmail.com>
2023-11-01 23:37:09 -0400

    deleted spaces startup.rs for rust fmt test

--- src/startup.rs
+++ src/startup.rs
@@ -15,8 +15,8 @@ pub fn run(listener: TcpListener) -> hyper::Result<Server<AddrIncoming, IntoMake
         .route("/", get(greet))
         .route("/:uri", get(greet))
         .route("/health_check", get(health_check))
-        .route("/subscriptions", post(subscribe));      
-
+        .route("/subscriptions", post(subscribe));
+    
     let server = axum::Server::from_tcp(listener)
         .expect("Could not instantiate TcpListener")
         .serve(app.into_make_service());
